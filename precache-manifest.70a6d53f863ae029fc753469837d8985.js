d8067a3cba29e79a10cd5511c1f74bf6c8d03fa1
aditya <priyadarshiaditya46@gmail.com>
2022-04-17 22:07:54 +0530

    Updates

---
+++ precache-manifest.70a6d53f863ae029fc753469837d8985.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d8a5561504b9caf13c4",
+    "url": "/TextUtils-React/static/css/main.e1300b41.chunk.css"
+  },
+  {
+    "revision": "2d8a5561504b9caf13c4",
+    "url": "/TextUtils-React/static/js/main.2d8a5561.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/TextUtils-React/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "2f7b561e41022ba6e414",
+    "url": "/TextUtils-React/static/js/2.2f7b561e.chunk.js"
+  },
+  {
+    "revision": "c1c4dd7bf6141002a83a",
+    "url": "/TextUtils-React/static/js/runtime~main.c1c4dd7b.js"
+  },
+  {
+    "revision": "15f7c90778131e3035338fc37fb63279",
+    "url": "/TextUtils-React/index.html"
+  }
+];
